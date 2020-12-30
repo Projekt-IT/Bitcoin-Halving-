e5a78bc14bb6c946d286193ea74db330c4d5e662
PaulEgeler <paul.egeler@web.de>
2020-12-30 10:15:51 +0100

    csv auslesen, graph erstellen

--- Halving file.py
+++ Halving file.py
@@ -1,5 +1,8 @@
-a = 1
-b = 2
-c = 3
-test = a + b + c
-print(test)
+import pandas as pd
+import matplotlib.pyplot as plt
+dataframe = pd.read_csv("BTC-USD.csv")
+dataframe_sorted = dataframe.sort_values(["Date"], ascending=True)
+dataframe_sorted_indexed = dataframe_sorted.set_index("Date")
+print(dataframe_sorted_indexed)
+dataframe_sorted_indexed.plot(figsize=(16,9))
+plt.show()
