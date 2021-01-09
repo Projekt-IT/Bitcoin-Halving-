922f7f34a6aa47fa65c5760c8429666433db504e
PaulEgeler <paul.egeler@web.de>
2021-01-09 17:11:15 +0100

    Graph Fortschritt

--- Halving file.py
+++ Halving file.py
@@ -3,10 +3,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 dataframe = pd.read_csv("BTC-USD.csv")
 
-dataframe_sorted = dataframe.sort_values(["Date"], ascending=True)
-dataframe_sorted_indexed = dataframe_sorted.set_index("Date")
-print(dataframe_sorted_indexed)
-dataframe_sorted_indexed.plot(figsize=(16,9))
+#nur Closing Stock Value anzeigen
+extr_dataframe = dataframe[["Date","Adj Close"]]
+print(extr_dataframe)
+
+#nach Datum sortieren
+extr_dataframe_sorted = extr_dataframe.sort_values(["Date"], ascending=True)
+extr_dataframe_sorted_indexed = extr_dataframe_sorted.set_index("Date")
+print(extr_dataframe_sorted_indexed)
+
+#Graph einführen
+extr_dataframe_sorted_indexed.plot(figsize=(16,9))
 
 #Schwarzer Hintergrund
 plt.style.use("dark_background")
@@ -17,7 +24,7 @@ plt.title("BTC-Aktie - Halvings")
 
 #Achsenbeschriftung
 plt.xlabel("Date")
-plt.ylabel("Close Stock Value")
+plt.ylabel("Closing Stock Value")
 
 #Gitternetz einfügen
 plt.grid()
@@ -28,12 +35,14 @@ plt.ylim(0, 25000)
 #Skalierung Abstufung y-Achse
 plt.yticks(np.arange(0, 25000, 1000))
 
-#Skalierung x-Achse
+#dateformat in monat/Jahr (mit DateFormatter), Eingabeformate (https://docs.python.org/3/library/datetime.html#strftime-and-strptime-behavior
+#date_format = DateFormatter("%b-%y")
+
+#Skalierung x-Achse (in Datetime umwandeln)
 
 
 #Vertikale  Linie an der Stelle 'x_pos' für Halvings
-#x_pos = 2016-01-03
-plt.vlines(x = 2016, color = "c", ymin = 0, ymax = 25000)
+plt.vlines(x = 2, color = "c", ymin = 0, ymax = 25000)
 #-> position in datetime bekommen
 
 plt.show()
